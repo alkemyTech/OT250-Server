84407fec455cdfa529364dd2e1d6903654733c75
Beniko19 <martin192012@gmail.com>
2022-07-15 11:19:30 -0300

    added the attributes and their annotations for the entity organization.

--- src/main/java/com/alkemy/ong/models/entity/OrganizationEntity.java
+++ src/main/java/com/alkemy/ong/models/entity/OrganizationEntity.java
@@ -5,8 +5,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
-
+import org.springframework.data.annotation.CreatedDate;
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "organizations")
@@ -16,5 +18,35 @@ import javax.persistence.*;
 @SQLDelete(sql = "UPDATE organizations SET soft_delete = true WHERE id=?")
 @Where(clause = "soft_delete = false")
 public class OrganizationEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Name may not be null")
+    private String name;
+
+    @NotNull(message = "Image may not be null")
+    private String image;
+
+    private String adress;
+
+    private Integer phone;
+
+    @NotNull(message = "Email may not be null")
+    private String email;
+
+    @Column(name = "welcome_text", columnDefinition = "TEXT")
+    @NotNull(message = "Welcome text may not be null")
+    private String welcomeText;
+
+    @Column(name = "about_us_text", columnDefinition = "TEXT")
+    private String aboutUsText;
+
+    @CreatedDate
+    private Timestamp timeStamp;
+
+    @Column(name = "is_deleted")
+    private boolean softDelete;
 
 }
