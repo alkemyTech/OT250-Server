bbe9be1afaa1e48fc7b25fae8533f0d67dc450ce
Beniko19 <martin192012@gmail.com>
2022-07-15 10:32:47 -0300

    added the annotations for the class

--- src/main/java/com/alkemy/ong/models/entity/OrganizationEntity.java
+++ src/main/java/com/alkemy/ong/models/entity/OrganizationEntity.java
@@ -1,4 +1,20 @@
 package com.alkemy.ong.models.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "organizations")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SQLDelete(sql = "UPDATE organizations SET soft_delete = true WHERE id=?")
+@Where(clause = "soft_delete = false")
 public class OrganizationEntity {
+
 }
