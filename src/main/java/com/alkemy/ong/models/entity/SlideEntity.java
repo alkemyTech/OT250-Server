3f04e281559d7b506bd1b294ee2ec7991df907cf
Beniko19 <martin192012@gmail.com>
2022-07-15 11:33:07 -0300

    added the attributes and annotations for the slide entity

--- src/main/java/com/alkemy/ong/models/entity/SlideEntity.java
+++ src/main/java/com/alkemy/ong/models/entity/SlideEntity.java
@@ -1,4 +1,34 @@
 package com.alkemy.ong.models.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "slides")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+//TODO @EntityListeners(AuditingEntityListener.class)
 public class SlideEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    private String text;
+
+    private String order;
+
+    @ManyToOne(cascade = CascadeType.ALL, targetEntity = OrganizationEntity.class)
+    @JoinTable(
+            name = "slides_organization",
+            joinColumns = @JoinColumn(name = "slide_id"),
+            inverseJoinColumns = @JoinColumn(name = "organization_id")
+    )
+    private OrganizationEntity organization;
 }
