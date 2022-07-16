d3745b777094197e9efc64346629892a42b0be19
Beniko19 <martin192012@gmail.com>
2022-07-16 10:14:26 -0300

    added the create method to the slide service interface.

--- src/main/java/com/alkemy/ong/service/ISlideService.java
+++ src/main/java/com/alkemy/ong/service/ISlideService.java
@@ -1,4 +1,12 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.models.request.SlideRequest;
+import com.alkemy.ong.models.response.SlideResponse;
+
+import java.io.IOException;
+
 public interface ISlideService {
+
+    SlideResponse create(SlideRequest slideRequest) throws IOException;
+
 }
