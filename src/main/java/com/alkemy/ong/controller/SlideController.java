e0844cdbd1bb55b945f55b4b7323b152c3c97774
Beniko19 <martin192012@gmail.com>
2022-07-16 12:02:38 -0300

    created the post mapping "create" in the slide controller.

--- src/main/java/com/alkemy/ong/controller/SlideController.java
+++ src/main/java/com/alkemy/ong/controller/SlideController.java
@@ -1,4 +1,31 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.models.request.SlideRequest;
+import com.alkemy.ong.models.response.SlideResponse;
+import com.alkemy.ong.service.ISlideService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("slides")
 public class SlideController {
+
+    @Autowired
+    private ISlideService slideService;
+
+    //TODO @PreAuthorize(ROLE_ADMIN)
+    @PostMapping
+    public ResponseEntity<SlideResponse> create(@RequestBody @Valid SlideRequest slideRequest) throws IOException {
+        SlideResponse responseSaved = slideService.create(slideRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseSaved);
+    }
+
 }
