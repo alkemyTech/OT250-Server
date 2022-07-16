bc9dfd454525edf1ee7b18b08c3cf2fb231b8c71
Beniko19 <martin192012@gmail.com>
2022-07-16 11:55:03 -0300

    added the annotation of Service.

--- src/main/java/com/alkemy/ong/service/impl/SlideServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/SlideServiceImpl.java
@@ -8,9 +8,9 @@ import com.alkemy.ong.models.response.SlideResponse;
 import com.alkemy.ong.repository.ISlideRepository;
 import com.alkemy.ong.service.ISlideService;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
 import java.io.IOException;
-
+@Service
 public class SlideServiceImpl implements ISlideService {
 
     @Autowired
