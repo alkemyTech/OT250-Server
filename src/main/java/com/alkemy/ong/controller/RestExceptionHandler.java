a2a8a773dc52a10ad8e4655780241877e418c791
Beniko19 <martin192012@gmail.com>
2022-07-16 13:03:57 -0300

    done the implementation of the method handler of paramNotFound class in the rest exception handler class.

--- src/main/java/com/alkemy/ong/controller/RestExceptionHandler.java
+++ src/main/java/com/alkemy/ong/controller/RestExceptionHandler.java
@@ -1,17 +1,27 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.exception.ParamNotFound;
+import com.alkemy.ong.models.response.ApiErrorResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Arrays;
+
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {ParamNotFound.class})
     protected ResponseEntity<Object> handleParamNotFound(RuntimeException ex, WebRequest request) {
-
+        ApiErrorResponse error = new ApiErrorResponse(
+                HttpStatus.BAD_REQUEST,
+                ex.getMessage(),
+                Arrays.asList("Param Not Found")
+        );
+        return handleExceptionInternal(ex, error, new HttpHeaders(), HttpStatus.BAD_REQUEST, request );
     }
 }
