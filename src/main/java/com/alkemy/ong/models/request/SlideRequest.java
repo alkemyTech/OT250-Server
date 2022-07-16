097c9f89ff478c579221e87fc8c3085e51b08396
Beniko19 <martin192012@gmail.com>
2022-07-16 10:08:06 -0300

    filled slide request dto with its attributes and annotations.

--- src/main/java/com/alkemy/ong/models/request/SlideRequest.java
+++ src/main/java/com/alkemy/ong/models/request/SlideRequest.java
@@ -1,4 +1,17 @@
 package com.alkemy.ong.models.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SlideRequest {
+    private String imageUrl;
+    private String text;
+    private Integer sort;
+    private Long organizationId;
 }
