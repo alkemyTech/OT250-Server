d11c30e24e19e992e45299c4a3b3530c1b1d12c3
Jose Luis Panero Grando <57608370+junx2000@users.noreply.github.com>
2022-07-16 21:02:46 -0300

    Add repository method

--- src/main/java/com/alkemy/ong/repository/UserRepository.java
+++ src/main/java/com/alkemy/ong/repository/UserRepository.java
@@ -4,7 +4,10 @@ import com.alkemy.ong.models.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository <UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
 
 }
