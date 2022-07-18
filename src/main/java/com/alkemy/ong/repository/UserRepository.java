7498a7e74a48be8a0867d191a14ace62e7820174
larrydaguilera <larrydaguilera@gmail.com>
2022-07-18 18:44:52 -0300

    Minor corrections

--- src/main/java/com/alkemy/ong/repository/UserRepository.java
+++ src/main/java/com/alkemy/ong/repository/UserRepository.java
@@ -2,9 +2,12 @@ package com.alkemy.ong.repository;
 
 import com.alkemy.ong.models.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository <UserEntity, Long> {
 
+    @Query("SELECT u FROM UserEntity u WHERE  u.email = :email")
+    public UserEntity findbyEmail(String email);
 }
