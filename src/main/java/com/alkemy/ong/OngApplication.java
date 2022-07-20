aa3530284d08efd6d7acca11d4bd56953e64c33a
ibarrondojosei <ibarrondojosei@gmail.com>
2022-07-20 17:38:08 -0300

    .

--- src/main/java/com/alkemy/ong/OngApplication.java
+++ src/main/java/com/alkemy/ong/OngApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication 
+@SpringBootApplication
 public class OngApplication {
 
 	public static void main(String[] args) {
