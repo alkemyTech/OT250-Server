17b238afc114e5726810e7188401778dc2901271
Beniko19 <martin192012@gmail.com>
2022-07-22 22:29:36 -0300

    implemented the method of getAllCategories

--- src/main/java/com/alkemy/ong/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/CategoryServiceImpl.java
@@ -1,13 +1,25 @@
 package com.alkemy.ong.service.impl;
 
+import com.alkemy.ong.models.entity.CategoryEntity;
+import com.alkemy.ong.models.mapper.CategoryMapper;
 import com.alkemy.ong.models.response.CategoryNameResponse;
+import com.alkemy.ong.repository.CategoryRepository;
 import com.alkemy.ong.service.ICategoryService;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import java.util.List;
 
+@Service
 public class CategoryServiceImpl implements ICategoryService {
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private CategoryMapper categoryMapper;
+
     public List<CategoryNameResponse> getAllCategories() {
-        return null;
+        List<CategoryEntity> categories = categoryRepository.findAll();
+        List<CategoryNameResponse> responsesList = categoryMapper.categoryEntities2Responses(categories);
+        return responsesList;
     }
 }
