9f20f298bdd5ea7a0df6463b80d6a37b294c2819
Beniko19 <martin192012@gmail.com>
2022-07-22 22:40:10 -0300

    created the method to get a category with its details  by the id provided.

--- src/main/java/com/alkemy/ong/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/CategoryServiceImpl.java
@@ -3,6 +3,7 @@ package com.alkemy.ong.service.impl;
 import com.alkemy.ong.models.entity.CategoryEntity;
 import com.alkemy.ong.models.mapper.CategoryMapper;
 import com.alkemy.ong.models.response.CategoryNameResponse;
+import com.alkemy.ong.models.response.CategoryResponse;
 import com.alkemy.ong.repository.CategoryRepository;
 import com.alkemy.ong.service.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,10 @@ public class CategoryServiceImpl implements ICategoryService {
         List<CategoryEntity> categories = categoryRepository.findAll();
         List<CategoryNameResponse> responsesList = categoryMapper.categoryEntities2Responses(categories);
         return responsesList;
+    }
+
+    @Override
+    public CategoryResponse getCategoryDetails(Long id) {
+
     }
 }
