d5031c54e25d2861689a32d35d16ed1a3d519eab
Beniko19 <martin192012@gmail.com>
2022-07-22 22:27:07 -0300

    added the class of the category service implementation

---
+++ src/main/java/com/alkemy/ong/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,13 @@
+package com.alkemy.ong.service.impl;
+
+import com.alkemy.ong.models.response.CategoryNameResponse;
+import com.alkemy.ong.service.ICategoryService;
+
+import java.util.List;
+
+public class CategoryServiceImpl implements ICategoryService {
+
+    public List<CategoryNameResponse> getAllCategories() {
+        return null;
+    }
+}
