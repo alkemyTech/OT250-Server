e299dc9d1fcae116be8ed764c8a6f6af2d2ccd9f
Beniko19 <martin192012@gmail.com>
2022-07-22 22:47:42 -0300

    created the method "categoryEntity2CategoryResponse" to get a category response from a category entity.

--- src/main/java/com/alkemy/ong/models/mapper/CategoryMapper.java
+++ src/main/java/com/alkemy/ong/models/mapper/CategoryMapper.java
@@ -2,6 +2,7 @@ package com.alkemy.ong.models.mapper;
 
 import com.alkemy.ong.models.entity.CategoryEntity;
 import com.alkemy.ong.models.response.CategoryNameResponse;
+import com.alkemy.ong.models.response.CategoryResponse;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -20,5 +21,13 @@ public class CategoryMapper {
         List<CategoryNameResponse> nameResponseList = categoryEntities.stream()
                 .map(c -> category2CategoryNameResponse(c)).collect(Collectors.toList());
         return nameResponseList;
+    }
+
+    public CategoryResponse categoryEntity2CategoryResponse(CategoryEntity categoryEntity) {
+        return CategoryResponse.builder().name(categoryEntity.getName())
+                .description(categoryEntity.getDescription())
+                .image(categoryEntity.getImage())
+                .timestamp(categoryEntity.getTimestamp())
+                .build();
     }
 }
