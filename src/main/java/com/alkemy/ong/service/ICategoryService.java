d063c1fadad7c40fcc16d6476951eddc7f9bc7bc
Beniko19 <martin192012@gmail.com>
2022-07-22 22:25:57 -0300

    created the method of getAllCategories in the interface to implements in the category service implementation

--- src/main/java/com/alkemy/ong/service/ICategoryService.java
+++ src/main/java/com/alkemy/ong/service/ICategoryService.java
@@ -1,4 +1,10 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.models.response.CategoryNameResponse;
+
+import java.util.List;
+
 public interface ICategoryService {
+
+    List<CategoryNameResponse> getAllCategories();
 }
