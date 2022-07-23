330e8c1cfee140a3c3eb2dc0dca3f294f9f4b4d8
Beniko19 <martin192012@gmail.com>
2022-07-22 22:13:19 -0300

    created the method to convert a category entity to a dto to do a response only with its name

--- src/main/java/com/alkemy/ong/models/mapper/CategoryMapper.java
+++ src/main/java/com/alkemy/ong/models/mapper/CategoryMapper.java
@@ -1,4 +1,17 @@
 package com.alkemy.ong.models.mapper;
 
+import com.alkemy.ong.models.entity.CategoryEntity;
+import com.alkemy.ong.models.response.CategoryNameResponse;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CategoryMapper {
+
+    public CategoryNameResponse category2CategoryNameResponse(CategoryEntity categoryEntity) {
+        return CategoryNameResponse.builder()
+                .name(categoryEntity.getName())
+                .build();
+    }
+
+
 }
