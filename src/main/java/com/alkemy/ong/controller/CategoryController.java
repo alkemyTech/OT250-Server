c63e3f76d5ad297aeb0d885d1c7bd737f7df9233
Beniko19 <martin192012@gmail.com>
2022-07-22 22:33:35 -0300

    done the method of getAllCategories

--- src/main/java/com/alkemy/ong/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -1,4 +1,25 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.models.response.CategoryNameResponse;
+import com.alkemy.ong.service.ICategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
 public class CategoryController {
+
+    @Autowired
+    private ICategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryNameResponse>> getAllCategories() {
+        List<CategoryNameResponse> categories = categoryService.getAllCategories();
+        return ResponseEntity.ok().body(categories);
+    }
 }
