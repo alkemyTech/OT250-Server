d6fe8080c6b06cd9f96b2281c7671b2b73713a2f
Beniko19 <martin192012@gmail.com>
2022-07-22 22:46:09 -0300

    added the builder annotation to be able to build the object.

--- src/main/java/com/alkemy/ong/models/response/CategoryResponse.java
+++ src/main/java/com/alkemy/ong/models/response/CategoryResponse.java
@@ -1,11 +1,13 @@
 package com.alkemy.ong.models.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
 
+@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
