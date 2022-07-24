7efba1baa84a85e8be61d1737540f6bc51f8380e
Beniko19 <martin192012@gmail.com>
2022-07-24 14:39:03 -0300

    added the basic class to create an admin user as a seeder

---
+++ src/main/java/com/alkemy/ong/auth/config/seeder/UserSeeder.java
@@ -0,0 +1,10 @@
+package com.alkemy.ong.auth.config.seeder;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class UserSeeder implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
