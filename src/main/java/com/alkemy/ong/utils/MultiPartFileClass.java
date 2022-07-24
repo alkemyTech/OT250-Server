6104f9670fb91a0a6cc7733d5c5553086befb852
Jose Luis Panero Grando <57608370+junx2000@users.noreply.github.com>
2022-07-24 19:36:46 -0300

    Impl. MultiPartFileClass

--- src/main/java/com/alkemy/ong/utils/MultiPartFileClass.java
+++ src/main/java/com/alkemy/ong/utils/MultiPartFileClass.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.utils;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,46 +9,49 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.Base64;
+import java.util.Date;
 
 
 public class MultiPartFileClass implements MultipartFile {
-
     private final byte[] FILE_CONTENT;
     private final String EXTENSION;
     private final String TYPE;
+    private final String FILE_NAME;
 
     public MultiPartFileClass (String header,String contents){
-
         this.FILE_CONTENT = Base64.getDecoder().decode(contents.getBytes(StandardCharsets.UTF_8));
         this.EXTENSION = header.split(";")[0].split("/")[1];
         this.TYPE = header.split(";")[0].split(":")[1];
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy_HH-mm-ss");
+        Date date = new Date();
+        this.FILE_NAME = formatter.format(date).toString();
     }
-
 
     @Override
     public String getName() {
-        return null;
+        return this.FILE_NAME;
     }
 
     @Override
     public String getOriginalFilename() {
-        return null;
+        return this.FILE_NAME+"."+this.EXTENSION;
     }
 
     @Override
     public String getContentType() {
-        return null;
+        return this.TYPE;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.FILE_CONTENT.length == 0 || this.FILE_CONTENT == null;
     }
 
     @Override
     public long getSize() {
-        return 0;
+        return this.FILE_CONTENT.length;
     }
 
     @Override
@@ -67,8 +71,5 @@ public class MultiPartFileClass implements MultipartFile {
 
     @Override
     public void transferTo(File file) throws IOException, IllegalStateException {
-
     }
-
-
 }
