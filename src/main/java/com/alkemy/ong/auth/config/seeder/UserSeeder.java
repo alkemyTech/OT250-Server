0be12a8bc65e5cb48f28c95e3ab87a2db25eafbf
Beniko19 <martin192012@gmail.com>
2022-07-24 14:55:39 -0300

    created the first implementation for the seeder to create an admin user, still needs to be tested or modified.

--- src/main/java/com/alkemy/ong/auth/config/seeder/UserSeeder.java
+++ src/main/java/com/alkemy/ong/auth/config/seeder/UserSeeder.java
@@ -1,10 +1,33 @@
 package com.alkemy.ong.auth.config.seeder;
 
+import com.alkemy.ong.models.entity.RoleEntity;
+import com.alkemy.ong.models.entity.UserEntity;
+import com.alkemy.ong.repository.RoleRepository;
+import com.alkemy.ong.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class UserSeeder implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
     @Override
     public void run(String... args) throws Exception {
+            createUserAdmin();
+    }
 
+    private void createUserAdmin() {
+        if (userRepository.findAll().isEmpty())
+            roleRepository.save(new RoleEntity(1L,"ADMIN", "admin role", null));
+            Set<RoleEntity> roles = (Set<RoleEntity>) roleRepository.findAll();
+            userRepository.save(new UserEntity(1L, "User1", "Subname",
+                    "email@email.com", "photo.png", "admin", roles,
+                    null, false));
     }
 }
