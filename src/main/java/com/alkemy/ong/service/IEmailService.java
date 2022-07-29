b6d1724a05f09cade0f22d47a35b222e257eba93
Beniko19 <martin192012@gmail.com>
2022-07-29 12:30:06 -0300

    done implementing the service to send an email.

--- src/main/java/com/alkemy/ong/service/IEmailService.java
+++ src/main/java/com/alkemy/ong/service/IEmailService.java
@@ -3,8 +3,10 @@ package com.alkemy.ong.service;
 import com.sendgrid.Email;
 import com.sendgrid.Mail;
 
+import java.io.IOException;
+
 public interface IEmailService {
-    void sendEmail(Mail email);
-    void getEmailReady(String to, String template, String content, String subject);
-    void checkFromRequest(String to, String from);
+    void sendEmail(Mail email) throws IOException;
+    void getEmailReady(String to, String template, String content, String subject) throws IOException;
+    void checkFromRequest(String to, String from) throws IOException;
 }
--- src/main/java/com/alkemy/ong/service/impl/EmailServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/EmailServiceImpl.java
@@ -2,23 +2,35 @@ package com.alkemy.ong.service.impl;
 
 import com.alkemy.ong.service.IEmailService;
 import com.alkemy.ong.utils.EmailUtils;
-import com.sendgrid.Content;
-import com.sendgrid.Email;
-import com.sendgrid.Mail;
+import com.sendgrid.*;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.IOException;
 
 public class EmailServiceImpl implements IEmailService {
-
+    @Value("${alkemy.ong.email.templateid}")
     private String templateId;
+    @Value("${alkemy.ong.email.sender}")
     private String organizationId;
+    @Value("${alkemy.ong.email.apikey}")
     private String sendGridKey;
+    @Value("${alkemy.ong.email.templateidcontact}")
     private String templateContactId;
 
-
-    public void sendEmail(Mail email) {
-
+    public void sendEmail(Mail email) throws IOException {
+        SendGrid sendGrid = new SendGrid(sendGridKey);
+        Request request = new Request();
+        try{
+            request.setMethod(Method.POST);
+            request.setEndpoint("/mail/send");
+            request.setBody(email.build());
+            Response response = sendGrid.api(request);
+        } catch (IOException ex) {
+            throw ex;
+        }
     }
 
-    public void getEmailReady(String to, String template, String contentValue, String subject) {
+    public void getEmailReady(String to, String template, String contentValue, String subject) throws IOException {
         Email fromEmail = new Email(organizationId);
         Email toEmail = new Email(to);
         Content content = new Content("text/html", contentValue);
@@ -27,7 +39,7 @@ public class EmailServiceImpl implements IEmailService {
         sendEmail(mail);
     }
 
-    public void checkFromRequest(String to, String from) {
+    public void checkFromRequest(String to, String from) throws IOException {
         if (from.toLowerCase().equals("userRegistered"))
             getEmailReady(to, templateId, EmailUtils.content("Bienvenido!", "Gracias por registrarse!"), "Organization Name");
         else
