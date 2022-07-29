d8a648e0ec53ccbc59398c850f51cad4b1066120
Beniko19 <martin192012@gmail.com>
2022-07-29 12:17:41 -0300

    done doing the method that verify from where it's being called and what it has to call back.

--- src/main/java/com/alkemy/ong/service/impl/EmailServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/EmailServiceImpl.java
@@ -1,6 +1,8 @@
 package com.alkemy.ong.service.impl;
 
 import com.alkemy.ong.service.IEmailService;
+import com.alkemy.ong.utils.EmailUtils;
+import com.sendgrid.Content;
 import com.sendgrid.Email;
 
 public class EmailServiceImpl implements IEmailService {
@@ -15,11 +17,18 @@ public class EmailServiceImpl implements IEmailService {
 
     }
 
-    public void getEmailReady(String to, String template, String content, String subject) {
-
+    public void getEmailReady(String to, String template, String contentValue, String subject) {
+        Email fromEmail = new Email(organizationId);
+        Email toEmail = new Email(to);
+        Content content = new Content("text/html", contentValue);
     }
 
     public void checkFromRequest(String to, String from) {
-
+        if (from.toLowerCase().equals("userRegistered"))
+            getEmailReady(to, templateId, EmailUtils.content("Bienvenido!", "Gracias por registrarse!"), "Organization Name");
+        else
+            getEmailReady(to, templateContactId, EmailUtils.content("Solicitud recibida",
+                    "Muchas gracias por \n " + "contactarte con nosotros \n " +
+                            "te mandaremos un mensaje a la brevedad"), "Somos mas Ong");
     }
 }
