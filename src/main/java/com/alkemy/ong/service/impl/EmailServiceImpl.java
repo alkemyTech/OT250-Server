96fc4a316668b532e2d5834b2c31da4757f1d1b1
Beniko19 <martin192012@gmail.com>
2022-07-29 11:27:16 -0300

    created the basic implementation of email service.

---
+++ src/main/java/com/alkemy/ong/service/impl/EmailServiceImpl.java
@@ -0,0 +1,15 @@
+package com.alkemy.ong.service.impl;
+
+import com.alkemy.ong.service.IEmailService;
+import com.sendgrid.Email;
+
+public class EmailServiceImpl implements IEmailService {
+
+    public void sendEmail(Email email) {
+
+    }
+
+    public void getEmailReady(String to, String from) {
+
+    }
+}
