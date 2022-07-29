72f3da01150e12e5b6a4cb759d8fe81393a61eb9
Beniko19 <martin192012@gmail.com>
2022-07-29 11:26:07 -0300

    added two initial methods to the interface of email service.

--- src/main/java/com/alkemy/ong/service/IEmailService.java
+++ src/main/java/com/alkemy/ong/service/IEmailService.java
@@ -1,4 +1,8 @@
 package com.alkemy.ong.service;
 
+import com.sendgrid.Email;
+
 public interface IEmailService {
+    void sendEmail(Email email);
+    void getEmailReady(String to, String from);
 }
